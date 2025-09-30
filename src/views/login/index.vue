3a19a9f584f21f6a42af2376bc8647ad31ee955b
MuzrabotIT-park <ozodbekpanjiyev0444@gmail.com>
2025-09-30 14:49:50 +0500

    login page change

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,20 +1,21 @@
 <template>
     <div class="flex justify-center items-center min-h-screen">
-      <div class="w-[380px] flex flex-col gap-4 items-center">
+      <div class="w-[380px] flex flex-col gap-8 items-center">
         <q-img
             src="/images/logo.png"
             width="330px"
         />
-        <div  class=" w-full h-[456px] flex flex-col gap-2 bg-white p-8  rounded-xl" >
+        <div  class=" w-full h-[456px] flex flex-col gap-6 bg-white p-8  rounded-xl" >
           <span class="text-xl font-bold" >Kirish</span>
           <div>
             <small class=" font-bold bg-[#E0E7FF33] ">LOGIN</small>
-            <q-input  class="bg-[#E0E7FF33] text-[#2E384D59]" outlined dense model-value=""/>
+            <q-input  placeholder="Login" class="bg-[#E0E7FF33] text-[#2E384D59]" outlined dense model-value=""/>
           </div>
           <div>
             <small class="font-bold">PAROL</small>
-            <q-input  class="bg-[#E0E7FF33] text-[#2E384D59]"  type="password" outlined dense model-value=""/>
+            <q-input  placeholder="password" class="bg-[#E0E7FF33] text-[#2E384D59]"  type="password" outlined dense model-value="password"/>
           </div>
+          <div class="g-recaptcha" :data-sitekey="siteKey"></div>
           <my-btn  name="Kirish" />
         </div>
       </div>
@@ -23,4 +24,18 @@
 <script setup lang="ts">
 
 import MyBtn from "@/components/myBtn.vue";
+import { onMounted } from "vue"
+
+const siteKey = " 6Ldbw9krAAAAAFFxwe-oiTw6fsfPgX9h20yWlBgx"  // Google’dan olganingizni yozing
+
+onMounted(() => {
+  // Google reCAPTCHA scriptini yuklash
+  const script = document.createElement("script")
+  script.src = "https://www.google.com/recaptcha/api.js"
+  script.async = true
+  script.defer = true
+  document.head.appendChild(script)
+})
+
+
 </script>
