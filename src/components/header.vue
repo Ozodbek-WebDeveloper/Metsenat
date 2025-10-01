e28a3f9645ad48bf35c9c19c81d219ac1474c023
MuzrabotIT-park <ozodbekpanjiyev0444@gmail.com>
2025-10-01 14:57:39 +0500

    create tab vs filter

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,7 +1,6 @@
 <template>
   <div  class="bg-white  flex flex-col ">
-    <div class="w-[90%] mx-auto py-2">
-  <h1>my test</h1>
+    <div class="w-[90%] flex flex-col gap-8 mx-auto py-2">
     <div class="flex justify-between  items-center">
       <div class="row items-center">
         <q-img
@@ -15,51 +14,75 @@
         </div>
       </div>
 
-      <div class=" flex  items-center ">
-        <div class="flex items-center bg-[#F1F1F3]">
-          <div class="q-mr-sm">{{ userName }}</div>
-          <q-avatar size="40px" class="bg-green rounded-md">
+      <div class=" flex  items-center gap-3 ">
+        <div class="flex gap-1 items-center p-1 pl-3 rounded-md bg-[#F1F1F3]">
+          <div class="text-sm font-bold">{{ userName }}</div>
+          <div  class="w-[30px] h-[30px] flex justify-center items-center bg-green rounded-md overflow-hidden">
             <q-icon name="person" color="white" />
-          </q-avatar>
+          </div>
         </div>
-        <q-btn dense flat round icon="logout" @click="logout" />
+        <q-icon class="cursor-pointer" size="25px"   name="logout" @click="logout" />
+      </div>
+    </div>
+
+      <div class="flex justify-between ">
+        <div class="w-[50%]">
+          <q-tabs
+              v-model="tab"
+              class=" text-blue-500 rounded-lg shadow-sm border-2 border-[#DDE3FB]"
+              active-color="white"
+              active-bg-color="primary"
+              indicator-color="transparent"
+              align="justify"
+              dense
+          >
+            <q-tab name="dashboard" label="DASHBOARD" class="font-bold" />
+            <q-tab name="homiylar" label="HOMIYLAR" class="font-bold border-x-2 border-[#DDE3FB]" />
+            <q-tab name="talabalar" label="TALABALAR" class="font-bold " />
+          </q-tabs>
+
+          <q-tab-panels v-model="tab" animated>
+            <q-tab-panel name="dashboard">
+              <div class="q-pa-md">📊 Dashboard content</div>
+            </q-tab-panel>
+
+            <q-tab-panel name="homiylar">
+              <div class="q-pa-md">🤝 Homiylar content</div>
+            </q-tab-panel>
+
+            <q-tab-panel name="talabalar">
+              <div class="q-pa-md">🎓 Talabalar content</div>
+            </q-tab-panel>
+          </q-tab-panels>
+        </div>
+        <div class="flex items-start gap-2">
+          <q-input
+              class="bg-[#E8E8E8]  rounded-md  w-[280px] text-lg" outlined dense placeholder="Izlash" no-error-icon v-model="search"
+          >
+            <template v-slot:prepend>
+              <q-icon name="search" />
+            </template>
+          </q-input>
+          <my-btn  no-caps icon="filter_alt" name="Filter" btn-class="!bg-[#EDF1FD] !text-[#3365FC]"/>
+        </div>
       </div>
 
     </div>
-
-    <q-tabs
-        v-model="tab"
-        class="q-mx-lg"
-        active-color="primary"
-        indicator-color="primary"
-        align="left"
-    >
-      <q-tab name="dashboard" label="DASHBOARD" />
-      <q-tab name="homiylar" label="HOMIYLAR" />
-      <q-tab name="talabalar" label="TALABALAR" />
-    </q-tabs>
-
-    <q-space />
-
-    </div>
-
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue"
+import MyBtn from "@/components/myBtn.vue";
 
 const tab = ref("dashboard")
 const userName = ref("Shohrux")
-
+const  search = ref("")
 const logout = () => {
   console.log("Chiqish bosildi")
-  // logout logikangizni shu yerda yozasiz
 }
 </script>
 
 <style scoped>
-.q-header {
-  height: 70px;
-}
+
 </style>
--- src/components/myBtn.vue
+++ src/components/myBtn.vue
@@ -1,12 +1,13 @@
 <template>
-<q-btn color="primary" :class="btnClass" class="bg-[#3366FF] w-full text-white " >
+<q-btn :icon="icon" color="primary" :class="btnClass" class="bg-[#3366FF] w-full text-white " >
   {{name ?? 'buttun'}}
 </q-btn>
 </template>
 <script setup lang="ts">
 interface IProps{
   name:string,
-  btnClass?:string
+  btnClass?:string,
+  icon?:string,
 }
  defineProps<IProps>()
 </script>
